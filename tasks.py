f13f1766e8ec5cc65c284af02955272a40ee9567
Andrey Starichenko <67385045+centroid457@users.noreply.github.com>
2023-01-22 11:26:44 +0300

    Create tasks.py

---
+++ tasks.py
@@ -0,0 +1,28 @@
+import re
+from typing import *
+import pytest
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n >= 2:
+            return 2 * self.climbStairs(n - 1)
+        else:
+            return n
+
+
+
+@pytest.mark.parametrize(
+    argnames="params,EXPECTED",
+    argvalues=[
+        ([1], 1),
+        ([2], 2),
+        ([3], 3),
+        ([4], 5),
+        ([5], 8),
+    ]
+)
+def test__solution(params,EXPECTED):
+    test_obj_link = Solution().climbStairs
+    result = test_obj_link(*params)
+    assert result == EXPECTED
