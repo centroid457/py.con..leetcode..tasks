28be06387092300ed5e191522a63a1def218af4c
admin <023>
2023-02-11 13:00:10 +0300

    summaryRanges

--- tasks.py
+++ tasks.py
@@ -2,41 +2,48 @@ import re
 from typing import *
 import pytest
 
+
 class Solution:
-    def convertToTitle(self, columnNumber: int) -> str:
-        ord_A = ord("A") - 1
-        base = 26
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        result = []
+        start = None
+        finish = None
 
-        result = ""
-        while columnNumber:
-            value = columnNumber % base
-            value = value or base
-            result = chr(value + ord_A) + result
+        def result_append():
+            if start == finish:
+                result.append(f"{start}")
+            else:
+                result.append(f"{start}->{finish}")
 
-            if columnNumber == base:
-                break
-            columnNumber = (columnNumber - 1)//base
+        for num in nums:
+            # INIT range
+            if start is None:
+                start = num
+                finish = num
+                continue
+
+            # WORK
+            if num > (finish + 1):
+                result_append()
+                start = num
+
+            finish = num
+
+        if start is not None:
+            result_append()
 
         return result
-
 
 
 @pytest.mark.parametrize(
     argnames="params,EXPECTED",
     argvalues=[
-        ([1], "A"),
-        ([7], "G"),
-        ([26], "Z"),
-        ([27], "AA"),
-        ([28], "AB"),
-        ([676], "YZ"),
-        ([701], "ZY"),
-        ([17576], "YYZ"),
-
+        ([[0,1,2,4,5,7]], ["0->2","4->5","7"]),
+        ([[0,2,3,4,6,8,9]], ["0","2->4","6","8->9"]),
     ]
 )
 def test__solution(params,EXPECTED):
-    test_obj_link = Solution().convertToTitle
+    test_obj_link = Solution().summaryRanges
     result = test_obj_link(*params)
     assert result == EXPECTED
 
