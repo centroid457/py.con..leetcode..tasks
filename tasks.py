42e91687c97fb8192626b610318b2e70cf37d5c3
admin <023>
2023-02-12 11:51:01 +0300

    thirdMax

--- tasks.py
+++ tasks.py
@@ -4,46 +4,41 @@ import pytest
 
 
 class Solution:
-    def summaryRanges(self, nums: List[int]) -> List[str]:
-        result = []
-        start = None
-        finish = None
-
-        def result_append():
-            if start == finish:
-                result.append(f"{start}")
-            else:
-                result.append(f"{start}->{finish}")
+    def thirdMax(self, nums: List[int]) -> int:
+        max_set: set = set()
 
         for num in nums:
-            # INIT range
-            if start is None:
-                start = num
-                finish = num
+            if len(max_set) < 3:
+                max_set.add(num)
                 continue
 
-            # WORK
-            if num > (finish + 1):
-                result_append()
-                start = num
+            max_set_min = min(max_set)
+            if num > max_set_min and num not in max_set:
+                max_set.remove(max_set_min)
+                max_set.add(num)
 
-            finish = num
+        if len(max_set) < 3:
+            return max(max_set)
+        else:
+            return min(max_set)
 
-        if start is not None:
-            result_append()
-
-        return result
 
 
 @pytest.mark.parametrize(
     argnames="params,EXPECTED",
     argvalues=[
-        ([[0,1,2,4,5,7]], ["0->2","4->5","7"]),
-        ([[0,2,3,4,6,8,9]], ["0","2->4","6","8->9"]),
+        ([[3,2,1]], 1),
+        ([[3,2,]], 3),
+        ([[3, 2, 2]], 3),
+        ([[2, 2, 2]], 2),
+        ([[2, ]], 2),
+
+        ([[5,2,4,1,3,6,0]], 4),
+        ([[1,2,2,5,3,5]], 2),
     ]
 )
 def test__solution(params,EXPECTED):
-    test_obj_link = Solution().summaryRanges
+    test_obj_link = Solution().thirdMax
     result = test_obj_link(*params)
     assert result == EXPECTED
 
