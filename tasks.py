72673732837eeaa216663c62944fe7e0851df9c1
Andrey Starichenko <67385045+centroid457@users.noreply.github.com>
2023-02-13 19:17:51 +0300

    addStrings i var reversed

--- tasks.py
+++ tasks.py
@@ -4,43 +4,44 @@ import pytest
 
 
 class Solution:
-    def PascalTriangle(self, numRows: int) -> List[List[int]]:
-        rows = []
-        row_prev = []
+    def addStrings(self, num1: str, num2: str) -> str:
+        result = ""
 
-        for row_index in range(numRows):
-            row = [1]
-            row_index_center = (row_index+2)//2 -1
+        num1_rev = num1[::-1]
+        num2_rev = num2[::-1]
 
-            for element_index in range(1, row_index+1):
-                if element_index == row_index_center and not row_index%2:
-                    row.append(row_prev[element_index] * 2)
-                    row.extend(row[0:-1][::-1])
-                    break
-                elif element_index <= row_index_center:
-                    row.append(row_prev[element_index - 1] + row_prev[element_index])
-                    continue
+        add_over = 0
+        while num1_rev or num2_rev or add_over:
+            try:
+                n1 = int(num1_rev[0])
+            except:
+                n1 = 0
+            try:
+                n2 = int(num2_rev[0])
+            except:
+                n2 = 0
 
-                row.extend(row[::-1])
-                break
+            num1_rev = num1_rev[1::]
+            num2_rev = num2_rev[1::]
 
-            row_prev = row
-            rows.append(row)
+            result_i = n1 + n2 + add_over
+            add_over = 1 if result_i > 9 else 0
+            result = str(result_i%10) + result
 
-        return rows
-
+        while result[0] == "0" and len(result)>1:
+            result = result[1::]
+        return result
 
 @pytest.mark.parametrize(
     argnames="params,EXPECTED",
     argvalues=[
-        ([1], [[1]]),
-        ([2], [[1],[1,1]]),
-        ([3], [[1],[1,1],[1,2,1]]),
-        ([4], [[1],[1,1],[1,2,1],[1,3,3,1]]),
-        ([5], [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]),
+        (["0", "0"], "0"),
+        (["0", "1"], "1"),
+        (["1", "2"], "3"),
+        (["11", "123"], "134"),
     ]
 )
 def test__solution(params,EXPECTED):
-    test_obj_link = Solution().PascalTriangle
+    test_obj_link = Solution().addStrings
     result = test_obj_link(*params)
     assert result == EXPECTED
